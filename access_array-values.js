84847614cc0e0e15e53e531da4dbfb52421801da
rouss789 <61165932+rouss789@users.noreply.github.com>
2020-02-19 00:07:03 -0600

    array.js
    En este ejerció  se crea una variable llamada comida, en donde se guarda un array, se selecciona  el arreglo pizza que se encuentra en la posición número dos y por ultimo se muestra en consola.

---
+++ access_array-values.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Areglos</title>
+  </head>
+  <body>
+
+    <script src="comida.js"></script>
+
+    var comida = " ['manzana', 'pizza', 'pera']";
+    document.write(comida[1]);
+    console.log("comida" + "[1]");
+    </script>
+  </body>
+</html>

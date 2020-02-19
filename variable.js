11e8dfadf956b3209e8687ace867341a60e5c387
rouss789 <61165932+rouss789@users.noreply.github.com>
2020-02-18 23:43:22 -0600

    crear variable ejemplo
    En este ejercicio se crea una variable de tipo string de nombre ejemplo y se muestra en consola

---
+++ variable.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Variable</title>
+  </head>
+  <body>
+
+    <script src="Variable.js"></script>
+
+    var ejemplo = "Mi segundo reto";
+    alert("Mi segundo reto:"+ejemplo);
+    console.log("Mi segundo reto" + "ejemplo");
+    </script>
+  </body>
+</html>
